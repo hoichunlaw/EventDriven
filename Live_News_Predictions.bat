9901d47bb3910d1756252fcf9ecad833f08bdbea
hoichunlaw <hoichunlaw@berkeley.edu>
2020-01-16 21:30:20 +0800

    scheduled commit

--- Live_News_Predictions.bat
+++ Live_News_Predictions.bat
@@ -1,6 +1,7 @@
+python D:\python\EventDriven\News_Headlines_Prediction.py
 cd /D D:\python\EventDriven\
 "C:\Program Files\Git\bin\git.exe" status
 "C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m 'scheduled commit'
+"C:\Program Files\Git\bin\git.exe" commit -m "scheduled commit"
 "C:\Program Files\Git\bin\git.exe" push origin master
 pause
